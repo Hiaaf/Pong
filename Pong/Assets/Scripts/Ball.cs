d0205d1f5beecb51f53c981443b2732bee029729
Hiago <hiaaf_@hotmail.com>
2024-03-27 15:32:50 -0300

    Pong! meio incompleto ainda

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -2,48 +2,25 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-  [SerializeField] private GameObject sprite;
-  [SerializeField] private float speed = 5;
-  private Vector2 _direction = new(-1, 1); // Starts to up left
-  private Vector2Int _hitBorder = Vector2Int.zero;
-  private Vector2 _objectSize;
-  private Vector2 _screenBounds;
+    [SerializeField] private Vector2 direction;
+    [SerializeField] private float speed;
 
-  private void Start()
-  {
-    _screenBounds = Camera.main.ScreenToWorldPoint(
-      new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-    _objectSize = sprite.transform.GetComponent<SpriteRenderer>().bounds.size;
-  }
-
-  private void Update()
-  {
-    transform.position += (Vector3)_direction.normalized * speed * Time.deltaTime;
-  }
-
-  private void LateUpdate()
-  {
-    Vector3 pos = transform.position;
-    if (pos.x - _objectSize.x / 2 <= -_screenBounds.x || pos.x + _objectSize.x / 2 >= _screenBounds.x)
+    private void Start()
     {
-      if (_hitBorder.x == 1) return;
-      _hitBorder.x = 1;
-      _direction.x *= -1;
-    }
-    else
-    {
-      _hitBorder.x = 0;
+        GetComponent<Rigidbody2D>().velocity = direction.normalized * speed;
     }
 
-    if (pos.y - _objectSize.y / 2 <= -_screenBounds.y || pos.y + _objectSize.y / 2 >= _screenBounds.y)
+    private static float HitFactor(Vector2 ballPos, Vector2 padlePos, float padleSize) =>
+        (ballPos.y - padlePos.y) / padleSize;
+    
+    private void OnCollisionEnter2D(Collision2D other)
     {
-      if (_hitBorder.y == 1) return;
-      _hitBorder.y = 1;
-      _direction.y *= -1;
+        if (!other.gameObject.name.StartsWith("Paddle")) return;
+        
+        float y = HitFactor(transform.position, other.transform.position,
+            other.collider.bounds.size.y);
+        direction = new Vector2(-direction.x, y);
+
+        GetComponent<Rigidbody2D>().velocity = direction.normalized * speed;
     }
-    else
-    {
-      _hitBorder.y = 0;
-    }
-  }
 }
